9d71e2113943fb44d25ea0ad990a8ce3719684d4
Mamanov Emil <emil.mamanov97@gmail.com>
2024-02-12 17:37:37 +0600

    added router and model for albums, added validation

--- index.ts
+++ index.ts
@@ -1,14 +1,33 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
+import config from "./config";
+
+import albumsRouter from "./routers/albumsRouter";
+import artistsRouter from "./routers/artistsRouter";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
-
 app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Server started on ${port} port!`);
-});
+app.use('/albums', albumsRouter);
+app.use('/artists', artistsRouter);
+
+const run = async () => {
+    await mongoose.connect(config.mongoose.db);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+        console.log('disconnected');
+    });
+};
+
+void run();
+
 
---
+++ routers/albumsRouter.ts
@@ -0,0 +1,50 @@
+import { Router, } from 'express';
+import Album from '../models/albumModel';
+
+const albumsRouter = Router();
+
+albumsRouter.get("/", async (req, res) => {
+    try {
+        let albums;
+        if (req.query.artist) {
+            albums = await Album.find({ artist: req.query.artist }).populate('artist', 'name');
+        } else {
+            albums = await Album.find().populate('artist', 'name');
+        }
+        return res.send(albums);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+albumsRouter.post("/", async (req, res) => {
+    const albumData = {
+        title: req.body.title,
+        artist: req.body.artist,
+        year: req.body.year,
+        coverImage: req.body.coverImage,
+    };
+
+    const album = new Album(albumData);
+
+    try {
+        await album.save();
+        return res.send(album);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+});
+
+albumsRouter.get("/:id", async (req, res) => {
+    try {
+        const album = await Album.findById(req.params.id).populate('artist', 'name');
+        if (!album) {
+            return res.status(404).json({ error: 'Album not found' });
+        }
+        return res.send(album);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+export default albumsRouter;
